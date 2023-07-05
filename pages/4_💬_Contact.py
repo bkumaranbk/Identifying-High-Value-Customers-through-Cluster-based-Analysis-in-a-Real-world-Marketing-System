567eedc380080864d0ae45fd598543010c0f13e3
Kumaran Bk <kumaranb_ct17033@stu.kln.ac.lk>
2023-07-06 00:33:06 +0530

    data.py updated and homepage.py updated

--- pages/4_💬_Contact.py
+++ pages/4_💬_Contact.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import smtplib
+from email.message import EmailMessage
 
 st.title("Contact Us")
 
@@ -13,9 +15,25 @@ if submit:
     if not name.strip() or not email.strip() or not message.strip():
         st.error("Please fill in all the required fields.")
     else:
-        # Process the form submission (you can add your own logic here)
-        st.success("Thank you for your message. We will get in touch with you soon.")
-        # You can also send an email or store the form data in a database
+        # Send email
+        msg = EmailMessage()
+        msg.set_content(message)  # Set the message content to the user's input
+
+        msg['Subject'] = 'New contact form submission'
+        msg['From'] = email  # Set the 'From' field to the user's email address
+        msg['To'] = 'bkumaran98@gmail.com'
+
+        try:
+            # Replace 'smtp.example.com' with your SMTP server address
+            with smtplib.SMTP('smtp.example.com', 587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                # Replace 'your_email@example.com' and 'your_password' with your email credentials
+                smtp.login('your_email@example.com', 'your_password')
+                smtp.send_message(msg)
+            st.success("Thank you for your message. We will get in touch with you soon.")
+        except Exception as e:
+            st.error(f"An error occurred while sending the email: {e}")
 
 # Contact information with emoji icons
 st.header("Get to Know Me & Stay Connected")
